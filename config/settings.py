cf1f81533b5744bb78fd796062003ae671256886
kikokhaledu <kikokhaled.u@gmail.com>
2022-10-14 18:20:46 +0400

    security added

--- config/settings.py
+++ config/settings.py
@@ -9,18 +9,22 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import environ
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+env = environ.Env()
+env_file_path = os.path.join(BASE_DIR, '.env')
+if os.path.exists(env_file_path):
+    environ.Env.read_env(env_file=env_file_path)
+    
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8b&4gj4x%@(v0h%8^#1tgm$$s-80rih5937318ntcyzdb4fh+6'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
